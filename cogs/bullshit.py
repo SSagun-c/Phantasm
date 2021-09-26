cbd0d5e3ad9a52d0946471f7a763be178f7003ff
Colin <colinpeiffer6@gmail.com>
2021-09-26 22:40:59 +0200

    owo

---
+++ cogs/bullshit.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import cooldown
+
+class Bullshit(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.lower() == 'owo':
+            await message.channel.send('What\'s this?')
+
+        await self.bot.process_commands(message)
+    
+def setup(bot):
+    bot.add_cog(Bullshit(bot))
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord import Intents
 from discord.ext import commands
 
 token = 'ODM0ODA5ODczNjM1MjEzMzUy.YIGTRw.x45N7EuinFZh3-ar9ywpRJdFRvo'
-client = commands.Bot(command_prefix='$', intents=Intents.all(), case_insensitive=True)
+client = commands.Bot(command_prefix='-', intents=Intents.all(), case_insensitive=True)
 client.remove_command('help')
 
 
@@ -13,16 +13,6 @@ client.remove_command('help')
 async def on_ready():      # Changes bots Activity status on discord
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="über meine Schüler"))
     print("client logged in as Mrs. Sensei")
-
-
-# Some bullshit stuff
-
-#@client.event
-#async def on_message(message):
-    #if 'owo' in message.content.lower():
-        #await message.channel.send("owo")
-   #await client.process_commands(message)
-
 
 
 for filename in os.listdir('./cogs'):
