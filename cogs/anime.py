1b6aec95750b2d7b11f97749530aeb681dbe0fdd
Colin <colinpeiffer6@gmail.com>
2021-09-27 22:01:54 +0200

    Fixing

--- cogs/anime.py
+++ cogs/anime.py
@@ -7,14 +7,5 @@ class Anime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="quote", aliases=["zitat"])
-    async def anime_quote(self, ctx):
-        async with aiohttp.ClientSession() as Session:
-            request = await Session.get('https://animechan.vercel.app/api/random')
-            aqjson = await request.json()
-        embed = discord.Embed(title=f"Anime: {aqjson['anime']}", description=f"Character: {aqjson['character']}", color=0xED61D3)
-        embed.add_field(name="Quote", value=aqjson['quote'])
-        await ctx.send(embed=embed)
-
 def setup(bot):
     bot.add_cog(Anime(bot))
