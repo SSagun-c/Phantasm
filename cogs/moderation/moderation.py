37c44ab20c182b838659cc14d9f56356966f241b
Colin <colinpeiffer6@gmail.com>
2021-10-23 00:17:30 +0200

    Additions to MAIN

---
+++ cogs/moderation/moderation.py
@@ -0,0 +1,22 @@
+import discord
+from discord import guild
+from discord.ext import commands
+
+class Reviver(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='delete-channel', help='delete a channel with the specified name')
+    async def delete_channel(ctx, channel_name):
+        # check if the channel exists
+        existing_channel = discord.utils.get(guild.channels, name=channel_name)
+        
+        # if the channel exists
+        if existing_channel is not None:
+            await existing_channel.delete()
+        # if the channel does not exist, inform the user
+        else:
+            await ctx.send(f'No channel named, "{channel_name}", was found')
+
+def setup(bot):
+    bot.add_cog(Reviver(bot))
